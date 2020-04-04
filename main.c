051b70822dcbf6d64e1c4145e519439d0b5621d1
Joe Trenshaw <jmtrenshaw@gmail.com>
2020-04-04 01:36:30 -0400

    description and init structure added

--- main.c
+++ main.c
@@ -1,3 +1,14 @@
+/*
+ * Project: Desk LED
+ * Description: A MSP432 project that controls LEDs for my desk.
+ * Author: Joe Trenshaw <jmtrenshaw@gmail.com>
+ *
+ * Date Created: 04-04-2020
+ *
+ * Last Modified: 04-04-2020
+ * Changes:
+ */
+
 #include "msp.h"
 
 
@@ -7,4 +18,11 @@
 void main(void)
 {
 	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;		// stop watchdog timer
+}
+
+
+
+void ledInit(void)
+{
+
 }
